cfbd4094f285f44bd8b06709dae3fb87fcfc5280
hyundang <hyunjin5697@yonsei.ac.kr>
2021-01-03 21:06:06 +0900

    url로 favicon, author 받아오기
    url로 favicon, author 받아오기

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -15,6 +15,8 @@ var data = {
   url: "",
   site_name: "",
   image: "",
+  author: "",
+  favicon: "",
 };
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
@@ -31,8 +33,11 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
         findOgTag(html, "description");
         findOgTag(html, "type");
         findOgTag(html, "site_name");
+        findAuthor(html);
+        findFavicon(html, url);
         console.log(data);
         console.log(data.image);
+        console.log(data.favicon);
       })
     });
   }
@@ -54,28 +59,65 @@ const getHTML = async (url) => {
 
 const findOgTag = (html, ogTag) => {
   // console.log('og:'+ogTag);
-  let location = html.indexOf('og:'+ogTag);
+  let location = html.indexOf(`"og:`+ogTag+`"`);
   if(location != -1){
     let start = html.indexOf(`content=`, location);
+    if(start != -1){
     let end = html.indexOf(`"`, start+9);
-    switch(ogTag){
-      case 'image':
-        data.image = html.substring(start+9, end);
-        break;
-      case 'type':
-        data.type = html.substring(start+9, end);
-        break;
-      case 'title':
-        data.title = html.substring(start+9, end);
-        break;
-      case 'description':
-        data.description = html.substring(start+9, end);
-        break;
-      case 'site_name':
-        data.site_name = html.substring(start+9, end);
-        break;
-      default:
-        console.log("default");
+      switch(ogTag){
+        case 'image':
+          data.image = html.substring(start+9, end);
+          break;
+        case 'type':
+          data.type = html.substring(start+9, end);
+          break;
+        case 'title':
+          data.title = html.substring(start+9, end);
+          break;
+        case 'description':
+          data.description = html.substring(start+9, end);
+          break;
+        case 'site_name':
+          data.site_name = html.substring(start+9, end);
+          break;
+        default:
+          console.log("default");
+      }
     }
   }
 }
+
+
+const findAuthor = (html) => {
+  let location = html.indexOf(`<meta name="author"`);
+  if(location != -1){
+    let start = html.indexOf(`content=`, location);
+    if(start != -1){
+      let end = html.indexOf(`"`, start+9);
+      data.author = html.substring(start+9, end);
+    }
+  }
+}
+
+const findFavicon = (html, url) => {
+  let location = html.indexOf(`<link rel="apple-touch-icon"`);
+  if(location != -1){
+    let start = html.indexOf(`href=`, location);
+    if(start != -1){
+      let end = html.indexOf(`"`, start+6);
+      let favicon_url = html.substring(start+6, end);
+      if(favicon_url[0] == '/' && favicon_url[1] != '/'){
+        favicon_url = sliceURL(url) + favicon_url;
+      }
+      data.favicon = favicon_url;
+    }
+  }
+}
+
+const sliceURL = (url) => {
+  let start = url.indexOf("/");
+  let end = url.indexOf("/", start+2);
+  let slice_url = url.substring(0, end);
+  return slice_url;
+}
+
