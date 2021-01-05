6a16588dcad0fd057843db53945561930a3284ed
juyun <ewq3167@gmail.com>
2021-01-06 00:30:00 +0900

    feat: 카드 퍼블리싱 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,49 +6,41 @@ import CardHover from './CardHover';
 import { listSelectState } from '../states/atom';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
-const ParkingView = ({ cookies }) => {
+const ParkingView = ({ cookies, idx }) => {
   const [listSelect, setListSelect] = useRecoilState(listSelectState);
 
   setTimeout(() => {
     setListSelect(false);
-  }, 1000); // 서버랑 통신하는 코드 (채린 생각)
+  }, 1000);
+
   return (
-    <Parking listSelect={listSelect}>
+    <Parking listSelect={listSelect} thumbnail={cookies.thumbnail}>
       <div className="parking--title">{cookies.directory}</div>
     </Parking>
   );
 };
 
-// p2) 파킹 인터렉션 2번 연속으로 들어가서, settimeout 콜백 활용해서 2번 넣거나, setinterval 사용하면 단순 반복으로 알고있어서..ㅋ..스바 근데 글케되면 이거 구조 아예 바뀔거같아서 시도 못하게써 ㅜ ->useState 사용해서 조건부 렌더링을 해야할듯
-// p3) 썸네일 들어왔는가 아닌가에 따라 파킹 사이즈도 달라져서, 썸네일 api받아오는 containar 에 이것도 넣는게 나을거같아.. ? 방법이 있으려나?
-
-// p5) 이유는 모르겠는데 처음 실행할 때 0.1초 실행됨
-// p6) 호버 안했을 때 갑자기 사라져 -> 썸네일 위에
-
-// 최종결론-> parking을 따로빼지 말고 하단container랑 합치고, 계속 서버는 받는 걸로. 타임 설정, 인터랙션? 등은 CSS로 구현 가능한 범위로 판단!
-
 const Parking = styled.div`
-  display: box;
-  position: absolute;
-  top: 0px;
-  left: 0px;
+  position: relative;
   width: 100%;
   height: 0;
   padding-bottom: ${props => (!props.thumbnail ? 'calc((160 / 360) * 100%)' : 'calc((220 / 360) * 100%)')};
   border-radius: 1.2rem;
   z-index: 10;
   background: rgba(0, 0, 0, 0.3);
+
   .parking--title {
-    font-family: Spoqa Han Sans Neo;
-    font-style: normal;
-    font-weight: bold;
     font-size: 1.8rem;
     line-height: 2.2rem;
-    display: flex;
-    align-items: center;
-    text-align: center;
     letter-spacing: -0.02em;
     color: #333333;
+    font-weight: 500;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #fff;
+    font-weight: 700;
   }
 `;
 
@@ -61,6 +53,7 @@ export default ({ cookies, idx }) => {
       {cardHover && !listSelect && <CardHover cookies={cookies} idx={idx} />}
       <Contents thumbnail={cookies.thumbnail}>
         <div className="thumbnail">{cardHover && listSelect && <ParkingView cookies={cookies} />}</div>
+        {cardHover && <ThumbnailHover thumbnail={cookies.thumbnail}> </ThumbnailHover>}
         <div className="title">{cookies.title}</div>
         <div className="content">{cookies.content}</div>
         <div className="profile">
@@ -80,11 +73,21 @@ const Container = styled.div`
   padding-top: calc(477 / 360 * 100%);
   background-color: ${theme.colors.white};
   border-bottom: 0.1rem solid #b2b2b2;
-  &:hover {
+  :hover {
     .title {
       text-decoration: underline;
     }
   }
+`;
+
+const ThumbnailHover = styled.div`
+  width: 100%;
+  height: 0;
+  padding-bottom: ${props => (!props.thumbnail ? 'calc((160 / 360) * 100%)' : 'calc((220 / 360) * 100%)')};
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 1.2rem;
+  position: absolute;
+  z-index: 9;
 `;
 
 const Contents = styled.div`
