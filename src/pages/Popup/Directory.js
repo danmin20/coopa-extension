8d21392f63f2ec025e1cc00e821d331e0d0e35bd
hyundang <hyunjin5697@yonsei.ac.kr>
2021-01-05 20:30:20 +0900

    feat: web clipper directory page 수정

--- src/pages/Popup/Directory.js
+++ src/pages/Popup/Directory.js
@@ -10,7 +10,9 @@ import useInput from '../../hooks/useInput';
 import { ClipperPageNumState } from '../../states/atom';
 
 // 나중에 api 연결
-const item = ['디자인', '마케팅', '프로그래밍', '기획', '쿠키파킹'];
+const item = ['디자인', '마케팅', '프로그래밍', '기획', '쿠키파킹','디자인', '마케팅', '프로그래밍', '기획', '쿠키파킹'];
+
+// 스크롤바 뿌옇게 되는거 방지??? 어떻게 하지...
 
 export default () => {
   const [pageNum, setPageNum] = useRecoilState(ClipperPageNumState);
@@ -18,7 +20,7 @@ export default () => {
   const searchText = useInput('');
 
   const handleBtnClick = () => {
-    setPageNum(2);
+   
   };
 
   const handleBackArrClick = () => {
@@ -26,7 +28,7 @@ export default () => {
   };
 
   const handleDirClick = () => {
-    setPageNum(3);
+    setPageNum(2);
   };
 
   const handleBtnMouseOver = () => {
@@ -40,25 +42,23 @@ export default () => {
   return (
     <Wrap>
       <HeadhWrap>
-        <BackArrow onClick={handleBackArrClick} src={back_arrow} />
-        <SearchBar>
-          <SearchIcon src={search_icon} />
-          <SearchInput value={searchText.value} onChange={searchText.onChange} />
-        </SearchBar>
+        <BackBtn onClick={handleBackArrClick}>
+          <BackArrow src={back_arrow} />
+        </BackBtn>
       </HeadhWrap>
-      <DirListWrap>
-        <DirList>
-          {item.map((item, idx) => (
-            <DirItem onClick={handleDirClick} key={idx}>
-              {item}
-            </DirItem>
-          ))}
-        </DirList>
-      </DirListWrap>
+      <DirList>
+        {item.map((item, idx) => (
+          <ReturnDirItems item={item} idx={idx}/>
+        ))}
+      </DirList>
       <BottomWrap>
+        <SearchInput
+          placeholder={"새 디렉토리 명을 입력하세요"} 
+          value={searchText.value} 
+          onChange={searchText.onChange} 
+        />
         <AddBtn isHover={isHover} onMouseOver={handleBtnMouseOver} onMouseLeave={handleBtnMouseLeave} onClick={handleBtnClick}>
-          <PlusIconWhite isHover={isHover} src={plus_icon_white} />
-          <PlusIcon isHover={isHover} src={plus_icon} />새 디렉토리 만들기
+          저장
         </AddBtn>
       </BottomWrap>
     </Wrap>
@@ -69,7 +69,6 @@ const Wrap = styled.div`
   width: 36.1rem;
   height: 35.1rem;
   border-radius: 1.2rem;
-  border: 1px solid black;
   background-color: #ffffff;
   margin: 0;
   padding: 0;
@@ -79,7 +78,7 @@ const Wrap = styled.div`
 `;
 
 const HeadhWrap = styled.div`
-  width: 36.1rem;
+  width: 36rem;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -87,42 +86,46 @@ const HeadhWrap = styled.div`
   margin-bottom: 0.7rem;
 `;
 
+const BackBtn = styled.div`
+  width: 4.2rem;
+  height: 4.2rem;
+  border: 0.1rem solid #C4C4C4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 1.2rem;
+`;
+
 const BackArrow = styled.img`
-  margin-right: 1.733rem;
-  margin-left: 2.2rem;
 `;
 
-const SearchBar = styled.div`
-  width: 29.2rem;
-`;
-
-const SearchIcon = styled.img`
-  width: 1.6rem;
-  height: 1.6rem;
-  position: absolute;
-  z-index: 10;
-  margin-left: 1.5rem;
-  margin-top: 1.1rem;
-`;
-
-const SearchInput = styled.input`
-  width: 25.1rem;
-  height: 3.9rem;
+const DirItemWrap = styled.div`
+  width: 28rem;
+  height: 4rem;
+  padding-left: 1.2rem;
+  padding-right: 1.6rem;
   border-radius: 0.8rem;
-  background-color: #f1f1f1;
-  appearance: none;
-  border: none;
-  outline: none;
-  padding-left: 3.5rem;
+  background: ${props => props.isHover ? '#F3F3F3' : 'white'};
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
 `;
-
-const DirListWrap = styled.div``;
 
 const DirItem = styled.div`
   font-size: 2rem;
   font-weight: 500;
   line-height: 2.4rem;
-  padding: 1.4rem 0;
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const DirItemHoverCircle = styled.div`
+  width: 1.4rem;
+  height: 1.4rem;
+  border-radius: 50%;
+  background: #FF7134;
+  display: ${props => props.isHover ? 'box' : 'none'};
 `;
 
 const DirList = styled.div`
@@ -130,21 +133,23 @@ const DirList = styled.div`
   grid-template-columns: 1fr;
   position: relative;
   overflow: auto;
-  width: 31rem;
+  width: 33rem;
   height: 20rem;
   /* width */
   ::-webkit-scrollbar {
-    width: 0.8rem;
+    width: 1rem;
   }
   /* Track */
   ::-webkit-scrollbar-track {
     background: none;
+    border: 0.1rem solid #C4C4C4;
+    border-radius: 0.5rem;
   }
   /* Handle */
   ::-webkit-scrollbar-thumb {
     background: #f1f1f1;
     border: 0.1rem solid #bfbfbf;
-    border-radius: 0.8rem;
+    border-radius: 0.5rem;
     box-sizing: border-box;
   }
   /* Handle on hover */
@@ -159,39 +164,81 @@ const BottomWrap = styled.div`
   height: 8.9rem;
   border-radius: 1.2rem;
   background-color: white;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const AddBtn = styled.div`
-  width: 31.9rem;
-  height: 4.9rem;
-  border: ${props => (props.isHover ? 'none' : '0.2rem solid #333333;')};
-  border-radius: 1rem;
+  /* background: linear-gradient(to top, white); */
   display: flex;
   flex-direction: row;
   align-items: center;
+  box-shadow: 0rem -1rem 1.5rem 0.8rem rgba(255, 255, 255, 0.75);
+  z-index: 5;
+`;
+
+const SearchInput = styled.input`
+  width: 21.4rem;
+  height: 4.6rem;
+  border-radius: 0.8rem;
+  background-color: #f3f3f3;
+  color: #B7B7B7;
+  font-size: 1.5rem;
+  line-height: 1.8rem;
+  font-weight: 500;
+  appearance: none;
+  border: none;
+  outline: none;
+  padding: 0 1.5rem;
+  margin-left: 1.5rem;
+  &::-webkit-input-placeholder{
+    color: #B7B7B7;
+    font-size: 1.5rem;
+    line-height: 1.8rem;
+    font-weight: 500;
+  }
+`;
+
+const AddBtn = styled.div`
+  width: 7.6rem;
+  height: 4.6rem;
+  border: ${props => (props.isHover ? 'none' : '0.2rem solid #FF7134')};
+  border-radius: 0.8rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   font-size: 1.6rem;
-  font-weight: ${props => (props.isHover ? '700' : '500 ')};
-  color: ${props => (props.isHover ? '#ffffff' : '#3d3d3d')};
-  background-color: ${props => (props.isHover ? '#FF7034' : '#ffffff')};
+  font-weight: 500;
+  color: ${props => (props.isHover ? '#ffffff' : '#FF7134')};
+  background-color: ${props => (props.isHover ? '#FF7134' : '#ffffff')};
   box-shadow: ${props => (props.isHover ? '0rem 0rem 1.2rem rgba(0, 0, 0, 0.13)' : 'none')};
+  margin-left: 1rem;
 `;
 
-const PlusIcon = styled.img`
-  width: 1.5rem;
-  height: 1.4rem;
-  margin-right: 1.2rem;
-  margin-left: 6.3rem;
-  display: ${props => (props.isHover ? 'none' : 'box')};
-`;
+const ReturnDirItems = ({item, idx}) => {
+  const [isHover, setIsHover] = useState(false);
+  const [pageNum, setPageNum] = useRecoilState(ClipperPageNumState);
 
-const PlusIconWhite = styled.img`
-  width: 1.5rem;
-  height: 1.4rem;
-  margin-right: 1.2rem;
-  margin-left: 6.3rem;
-  display: ${props => (props.isHover ? 'box' : 'none')};
-`;
+  const handleDirClick = () => {
+    setPageNum(2);
+  };
+
+  const handleMouseMove = () => {
+    setIsHover(true);
+  }
+
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  }
+
+  return(
+    <DirItemWrap
+      onClick={handleDirClick}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      isHover={isHover}
+      key={idx}
+    >
+      <DirItem>
+        {item}
+      </DirItem>
+      <DirItemHoverCircle isHover={isHover} />
+    </DirItemWrap>
+  )
+}
--- src/pages/Popup/Popup.js
+++ src/pages/Popup/Popup.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import { ClipperPageNumState } from '../../states/atom';
-import AddDir from './AddDir';
 import Finish from './Finish';
 import Main from './Main';
 import Directory from './Directory';
 import Login from './Login';
-
 
 export default () => {
   const [pageNum, setPageNum] = useRecoilState(ClipperPageNumState);
@@ -17,11 +15,9 @@ export default () => {
     case 1:
       return <Directory />;
     case 2:
-      return <AddDir />;
-    case 3:
       return <Finish />;
     default:
       return <div>error</div>;
   }
-  // return <Login />
+  // return <Directory />
 };
