ff4d1eb15a038aa86fcb3a664a0ca1cadf06ec52
lee jeongmin <danmin20@khu.ac.kr>
2021-01-09 01:31:43 +0900

    fix: build error

--- src/lib/api/cookieApi.js
+++ src/lib/api/cookieApi.js
@@ -4,20 +4,20 @@ const baseURL = 'https://www.cookieparking.com';
 
 // 문서에 url cookies 로 바뀔 예정이라고 되어 있어서 수정함
 const postCookie = async (headers, body) => {
-    try {
-      const { data } = await axios({
-        baseURL,
-        url: '/cookies',
-        method: 'post',
-        headers: headers,
-        data: body
-      });
-      console.log('[SUCCESS] POST COOKIES', data);
-      return data;
-    } catch (e) {
-      console.error('[FAIL] POST COOKIES', error);
-      return e;
-    }
+  try {
+    const { data } = await axios({
+      baseURL,
+      url: '/cookies',
+      method: 'post',
+      headers: headers,
+      data: body
+    });
+    console.log('[SUCCESS] POST COOKIES', data);
+    return data;
+  } catch (e) {
+    console.error('[FAIL] POST COOKIES', error);
+    return e;
+  }
 };
 
 const getCookies = async headers => {
@@ -32,22 +32,6 @@ const getCookies = async headers => {
     return data;
   } catch (e) {
     console.error('[FAIL] GET COOKIES', error);
-    return e;
-  }
-};
-
-const getCookiesSearch = async (headers, word) => {
-  try {
-    const { data } = await axios({
-      baseURL,
-      url: `/cookies/search?word=${word}`,
-      method: 'get',
-      headers
-    });
-    console.log('[SUCCESS] GET SEARCH COOKIES', data);
-    return data;
-  } catch (e) {
-    console.error('[FAIL] GET SEARCH COOKIES', error);
     return e;
   }
 };
